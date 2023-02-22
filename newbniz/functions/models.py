ebd92c4d2a147fd44b20e14b9f12943543a0b113
Luciano Chavarria <62488915+Luciano890@users.noreply.github.com>
2023-02-22 03:17:49 +0000

    feat: add functions models.

--- newbniz/functions/models.py
+++ newbniz/functions/models.py
@@ -1,3 +1,21 @@
+"""Models for functions app"""
 from django.db import models
 
-# Create your models here.
+class Function(models.Model):
+    """Functions model"""
+    name = models.CharField(max_length=30)
+    function = models.CharField(max_length=30)
+    created_at = models.DateTimeField("date published", auto_now_add=True)
+
+    def __str__(self):
+        return f"Function Name: {self.name}, Function: {self.function}"
+
+class FunctionParameter(models.Model):
+    """Function parameters model"""
+    function = models.ForeignKey(Function, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    value = models.FloatField()
+    created_at = models.DateTimeField("date published", auto_now_add=True)
+
+    def __str__(self):
+        return f"Function Parameter Name: {self.name}, Value: {self.value}"
